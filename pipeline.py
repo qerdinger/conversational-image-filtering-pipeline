3ae3cea7046cfef27c18dc45fb3ee9c702e7def9
qerdinger <91551241+QuentinCraft@users.noreply.github.com>
2026-05-09 11:02:19 +0200

    feat: adds indexication process function + ELK class helper

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,8 @@
 from transformers import CLIPProcessor, CLIPModel
 from PIL import Image
 import torch
+from elasticsearch import Elasticsearch
+import os
 
 model = CLIPModel.from_pretrained(
     "openai/clip-vit-base-patch32"
@@ -10,7 +12,7 @@ processor = CLIPProcessor.from_pretrained(
     "openai/clip-vit-base-patch32"
 )
 
-def generate_embedding(image_path):
+def compute_image_embedding(image_path):
     image = Image.open(image_path).convert("RGB")
 
     inputs = processor(
@@ -26,8 +28,127 @@ def generate_embedding(image_path):
         keepdim=True
     )
 
-    embedding = image_features[0].cpu().numpy().tolist()
+    return image_features[0].cpu().numpy().tolist()
 
-    return embedding
+print(compute_image_embedding("./samples/skills-learning.png"))
 
-print(generate_embedding("./samples/skills-learning.png"))
+def compute_prompt_embedding(prompt):
+    inputs = processor(
+        text=[prompt],
+        return_tensors="pt",
+        padding=True
+    )
+
+    text_features = model.get_text_features(**inputs)
+
+    text_features = text_features / text_features.norm(
+        dim=-1,
+        keepdim=True
+    )
+
+    return text_features[0].detach().numpy()
+
+print(compute_prompt_embedding("photo with a computer"))
+
+class ELK:
+    def __init__(self, address="http://localhost:9200"):
+        self.address = address
+        self.cluster = Elasticsearch(self.address)
+        self.index = "cifp"
+        self.body = {
+        "mappings": {
+            "properties": {
+                "filepath": {
+                    "type": "keyword"
+                },
+                "filename": {
+                    "type": "text"
+                },
+                "embedding": {
+                    "type": "dense_vector",
+                    "dims": 512,
+                    "index": True,
+                    "similarity": "cosine"
+                }
+            }
+        }
+    }
+        self.setup()
+        
+    def setup(self):
+        print("ELK/SETUP> Running...")
+        if self._index_exist(self.index):
+            print("ELK/SETUP> Already Setup!")
+            return
+        print("ELK/SETUP> Configuring...")
+        try:
+            self._index_create(self.index, self.body)
+        except:
+            raise "ELK/SETUP> Failed!"
+            
+        if self._index_exist(self.index):
+            print("ELK/SETUP> Correctly Setup!")
+            return
+        raise "ELK/SETUP> Failed!"
+        
+
+    def _index_exist(self, name):
+        return self.cluster.indices.exists(index=name)
+
+    def _index_create(self, name, body):
+        self.cluster.indices.create(index=name, body=body)
+    
+    def set_index(self, document):
+        self.cluster.index(
+            index=self.index,
+            document=document
+        )
+        
+
+elk_cluster = ELK()
+
+def index_images(fpath):
+    supported_extensions = (
+        ".jpg",
+        ".jpeg",
+        ".png",
+    )
+
+    for filename in os.listdir(fpath):
+
+        if not filename.lower().endswith(
+            supported_extensions
+        ):
+            continue
+
+        filepath = os.path.join(
+            fpath,
+            filename
+        )
+
+        print(f"indexing: {filepath}")
+
+        try:
+            print("computing embedding...")
+            embedding = compute_image_embedding(
+                filepath
+            )
+
+            print("assembling document...")
+            document = {
+                "filepath": filepath,
+                "filename": filename,
+                "embedding": embedding
+            }
+
+            print("inserting into db...")
+            print(f"preview document : {document}")
+            elk_cluster.set_index(document)
+
+            print(f"indexed: {filename}")
+
+        except Exception as ex:
+            print(f"err: {filename}")
+            print(ex)
+            
+index_images("./samples")
